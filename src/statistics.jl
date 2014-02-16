12423dec7f4dc5451034109d18cd36af18f98353
Westley <WestleyArgentum@gmail.com>
2014-02-16 14:03:28 -0800

    refactor statistics functions

--- src/statistics.jl
+++ src/statistics.jl
@@ -6,23 +6,8 @@ function contributors(owner, repo, attempts = 3; auth = AnonymousAuth(), options
 end
 
 function contributors(auth::Authorization, owner, repo, attempts = 3; headers = Dict(), options...)
-    authenticate_headers(headers, auth)
-
-    for a in attempts:-1:0
-        r = get(URI(API_ENDPOINT; path = "/repos/$owner/$repo/stats/contributors");
-                headers = headers,
-                options...)
-
-        handle_error(r)
-
-        if r.status == 200
-            return JSON.parse(r.data)
-        end
-
-        sleep(2.0)
-    end
-
-    throw(GitHubError("Unsuccessfully attempted to retrieve contributors $attempts times."))
+    r = attempt_stats_request(auth, owner, repo, "contributors", attempts; headers = headers, options...)
+    JSON.parse(r.data)
 end
 
 
@@ -31,21 +16,29 @@ function commit_activity(owner, repo, attempts = 3; auth = AnonymousAuth(), opti
 end
 
 function commit_activity(auth::Authorization, owner, repo, attempts = 3; headers = Dict(), options...)
+    r = attempt_stats_request(auth, owner, repo, "commit_activity", attempts; headers = headers, options...)
+    JSON.parse(r.data)
+end
+
+
+# Utility -------
+
+function attempt_stats_request(auth, owner, repo, stat, attempts; headers = Dict(), options...)
     authenticate_headers(headers, auth)
 
     for a in attempts:-1:0
-        r = get(URI(API_ENDPOINT; path = "/repos/$owner/$repo/stats/commit_activity");
+        r = get(URI(API_ENDPOINT; path = "/repos/$owner/$repo/stats/$stat");
                 headers = headers,
                 options...)
 
         handle_error(r)
 
         if r.status == 200
-            return JSON.parse(r.data)
+            return r
         end
 
         sleep(2.0)
     end
 
-    throw(GitHubError("Unsuccessfully attempted to retrieve commit activity $attempts times."))
+    throw(GitHubError("Unsuccessfully attempted to retrieve $stat $attempts times."))
 end
