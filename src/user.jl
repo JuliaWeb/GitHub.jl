e46eb8ec9395ec52bb43ee3fc03777181ce56788
Westley <WestleyArgentum@gmail.com>
2014-02-17 20:21:35 -0800

    User type

--- src/user.jl
+++ src/user.jl
@@ -1,4 +1,61 @@
 
+# Types -------
+
+type User
+    id
+    email
+    name
+    login
+    location
+    gravatar_id
+
+    public_repos
+    owned_private_repos
+    total_private_repos
+
+    public_gists
+    private_gists
+
+    followers
+    following
+    collaborators
+
+    company
+    hireable
+
+    updated_at
+    created_at
+
+    plan
+    user_type
+    site_admin
+    disk_usage
+
+    function User(data::Dict)
+        new(get(data, "id", nothing),
+            get(data, "email", nothing),
+            get(data, "name", nothing),
+            get(data, "login", nothing),
+            get(data, "location", nothing),
+            get(data, "gravatar_id", nothing),
+            get(data, "public_repos", nothing),
+            get(data, "owned_private_repos", nothing),
+            get(data, "total_private_repos", nothing),
+            get(data, "public_gists", nothing),
+            get(data, "private_gists", nothing),
+            get(data, "followers", nothing),
+            get(data, "following", nothing),
+            get(data, "collaborators", nothing),
+            get(data, "company", nothing),
+            get(data, "hireable", nothing),
+            get(data, "updated_at", nothing),
+            get(data, "created_at", nothing),
+            get(data, "plan", nothing),
+            get(data, "type", nothing),
+            get(data, "site_admin", nothing),
+            get(data, "disk_usage", nothing))
+    end
+end
 
 # Interface -------
 
@@ -15,5 +72,5 @@ function user(auth::Authorization, username; headers = Dict(), options...)
 
     handle_error(r)
 
-    data = JSON.parse(r.data)
+    User(JSON.parse(r.data))
 end
