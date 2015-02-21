dfaa2561a33f99b390d3eda71c0350d3df0a210e
WestleyArgentum <WestleyArgentum@gmail.com>
2015-02-20 20:35:11 -0500

    formatting

--- src/utils.jl
+++ src/utils.jl
@@ -15,33 +15,38 @@ end
 
 # Parses a Link header into the links it represents
 function parse_link_header(s::String)
-  results = Dict{String,URI}()
-  # <url>; rel="name", <url2>; rel="name2", ...
-  for m in eachmatch(r"<([^>]+)>; rel=\"(\w+)\",? ?", s)
-    results[m.captures[2]] = URI(m.captures[1])
-  end
-  return results
+    results = Dict{String, URI}()
+
+    # <url>; rel="name", <url2>; rel="name2", ...
+    for m in eachmatch(r"<([^>]+)>; rel=\"(\w+)\",? ?", s)
+        results[m.captures[2]] = URI(m.captures[1])
+    end
+
+    return results
 end
 
 # Given a URI, fetches it
 # Returns the response and the next link, if any.
-function next_page(next_link::URI,headers::Dict{String,String})
-  r = get(next_link;headers=headers)
-  handle_error(r)
-  links = parse_link_header(r.headers["Link"])
-  if haskey(links, "next")
-    return (r, links["next"])
-  end
-  return (r, nothing)
+function next_page(next_link::URI, headers::Dict{String, String})
+    r = get(next_link;headers = headers)
+    handle_error(r)
+
+    links = parse_link_header(r.headers["Link"])
+    if haskey(links, "next")
+        return (r, links["next"])
+    end
+
+    return (r, nothing)
 end
 
 # To make pages iterable
 type Pager
-  initial_link::URI
-  headers::Dict{String,String}
+    initial_link::URI
+    headers::Dict{String, String}
 end
+
 Base.start(p::Pager) = p.initial_link
-Base.next(p::Pager, l::URI) = next_page(l,p.headers)
+Base.next(p::Pager, l::URI) = next_page(l, p.headers)
 Base.done(p::Pager, l::URI) = false
 Base.done(p::Pager, l::Nothing) = true
 
@@ -50,32 +55,36 @@ Base.done(p::Pager, l::Nothing) = true
 # result_limit is the desired number of results; the default (-1) indicates all results
 # per_page is the number of results to expect per page; default is 30, GitHub's default
 # Returns an Array of Responses (1 per page)
-function get_pages(u::URI,result_limit::Int=-1,per_page::Int=30;headers = Dict(),options...)
-  r = get(u;headers=headers,options...)
-  handle_error(r)
+function get_pages(u::URI, result_limit::Int = -1, per_page::Int = 30; headers = Dict(), options...)
+    r = get(u; headers = headers, options...)
+    handle_error(r)
 
-  pages = [r]
-  links = haskey(r.headers,"Link") ? parse_link_header(r.headers["Link"]) : nothing
-  if links != nothing && haskey(links,"next") && (result_limit < 0 || per_page < result_limit)
-    p = Pager(links["next"],headers)
-    for page in p
-      push!(pages,page)
-      if result_limit > 0 && length(pages) * 30 >= result_limit
-        break
-      end
+    pages = [r]
+    links = haskey(r.headers, "Link") ? parse_link_header(r.headers["Link"]) : nothing
+
+    if links != nothing && haskey(links, "next") && (result_limit < 0 || per_page < result_limit)
+        p = Pager(links["next"], headers)
+        for page in p
+            push!(pages, page)
+
+            if result_limit > 0 && length(pages) * 30 >= result_limit
+                break
+            end
+        end
     end
-  end
-  return pages
+
+    return pages
 end
 
 # get_paged gives you an Array{Response,1}
 # get_items_from_pages turns that into an Array{Dict,1}
 # each Dict is one of the items in the paginated list of results
 function get_items_from_pages(pages)
-  results = Dict[]
-  for page in pages
-    parsed = JSON.parse(page.data)
-    append!(results,parsed)
-  end
-  return results
+    results = Dict[]
+    for page in pages
+        parsed = JSON.parse(page.data)
+        append!(results, parsed)
+    end
+
+    return results
 end
