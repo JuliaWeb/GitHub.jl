ade85ec36b2aef24990d6c32ec0bfb11a5e8b0b3
jlewis91@me.com <jeremiah.lewis@razorfish.de>
2015-03-22 09:17:28 +0100

    added FactCheck & two tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,26 @@
-using GitHub
-using Base.Test
+using GitHub, FactCheck
 
 ############################################## contents.jl ##############################################
 
 # gh_auth = authenticate("123...")
 
-# a = readme(gh_auth, owner, repo)
-# a = contents(gh_auth, owner, repo)
-#
-# a = create_file(gh_auth, owner, repo, "test.json", "message",
-#                 {"content"=>"test"})
-# b = update_file(gh_auth, owner, repo, "test.json", "message",
-#                 a["content"].sha, {"content"=>"test1", "new"=> "key"})
-# c = delete_file(gh_auth, owner,repo, "test.json",
-#                 b["content"].sha, "message")
+facts("Read GitHub.jl Repo Files") do
+  owner = "WestleyArgentum"
+  repo = "GitHub.jl"
+  context("Readme function returns readme file data") do
+    readme_data = readme(gh_auth, owner, repo)
+    @fact typeof(readme_data) => File
+  end
+
+  context("Contents function returns list of files") do
+    contents_data = contents(gh_auth, owner, repo)
+    @fact typeof(contents_data[1]) => File
+  end
+
+  # a = create_file(gh_auth, owner, repo, "test.json", "message",
+  #                 {"content"=>"test"})
+  # b = update_file(gh_auth, owner, repo, "test.json", "message",
+  #                 a["content"].sha, {"content"=>"test1", "new"=> "key"})
+  # c = delete_file(gh_auth, owner,repo, "test.json",
+  #                 b["content"].sha, "message")
+end
