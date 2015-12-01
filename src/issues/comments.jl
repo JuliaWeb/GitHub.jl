34f455a0b48dbbcdd47172833253ee41b5f42fd9
Jarrett Revels <jarrettrevels@gmail.com>
2015-11-30 19:30:26 -0500

    reworked the comment-related methods to allow for interaction with commit comments

--- src/issues/comments.jl
+++ src/issues/comments.jl
@@ -26,34 +26,74 @@ Comment(id::Real) = Comment(Dict("id" => id))
 
 namefield(comment::Comment) = comment.id
 
+kind_err_str(kind) = ("Error building comment request: :$kind is not a valid kind of comment.\n"*
+                      "The only valid comment kinds are: :issue, :review, :commit")
 ###############
 # API Methods #
 ###############
 
-commentpath(review) = review ? "pulls" : "issues"
-
-function comment(repo, comment_obj, review = false; options...)
-    path = "/repos/$(name(repo))/$(commentpath(review))/comments/$(name(comment_obj))"
+function comment(repo, item, kind = :issue; options...)
+    if kind == :issue
+        path = "/repos/$(name(repo))/issues/comments/$(name(item))"
+    elseif kind == :review
+        path = "/repos/$(name(repo))/pulls/comments/$(name(item))"
+    elseif kind == :commit
+        path = "/repos/$(name(repo))/comments/$(name(item))"
+    else
+        error(kind_err_str(kind))
+    end
     return Comment(gh_get_json(path; options...))
 end
 
-function comments(repo, issue_or_pr, review = false; options...)
-    path = "/repos/$(name(repo))/$(commentpath(review))/$(name(issue_or_pr))/comments"
+function comments(repo, item, kind = :issue; options...)
+    if kind == :issue
+        path = "/repos/$(name(repo))/issues/$(name(item))/comments"
+    elseif kind == :review
+        path = "/repos/$(name(repo))/pulls/$(name(item))/comments"
+    elseif kind == :commit
+        path = "/repos/$(name(repo))/commits/$(name(item))/comments"
+    else
+        error(kind_err_str(kind))
+    end
     results, page_data = gh_get_paged_json(path; options...)
     return map(Comment, results), page_data
 end
 
-function create_comment(repo, issue_or_pr, review = false; options...)
-    path = "/repos/$(name(repo))/$(commentpath(review))/$(name(issue_or_pr))/comments"
-    return Comment(gh_get_json(path; options...))
+function create_comment(repo, item, kind = :issue; options...)
+    if kind == :issue
+        path = "/repos/$(name(repo))/issues/$(name(item))/comments"
+    elseif kind == :review
+        path = "/repos/$(name(repo))/pulls/$(name(item))/comments"
+    elseif kind == :commit
+        path = "/repos/$(name(repo))/commits/$(name(item))/comments"
+    else
+        error(kind_err_str(kind))
+    end
+    return Comment(gh_post_json(path; options...))
 end
 
-function edit_comment(repo, comment, review = false; options...)
-    path = "/repos/$(name(repo))/$(commentpath(review))/comments/$(name(comment))"
+function edit_comment(repo, item, kind = :issue; options...)
+    if kind == :issue
+        path = "/repos/$(name(repo))/issues/comments/$(name(item))"
+    elseif kind == :review
+        path = "/repos/$(name(repo))/pulls/comments/$(name(item))"
+    elseif kind == :commit
+        path = "/repos/$(name(repo))/comments/$(name(item))"
+    else
+        error(kind_err_str(kind))
+    end
     return Comment(gh_patch_json(path; options...))
 end
 
-function delete_comment(repo, comment, review = false; options...)
-    path = "/repos/$(name(repo))/$(commentpath(review))/comments/$(name(comment))"
+function delete_comment(repo, item, isreview = false; options...)
+    if kind == :issue
+        path = "/repos/$(name(repo))/issues/comments/$(name(item))"
+    elseif kind == :review
+        path = "/repos/$(name(repo))/pulls/comments/$(name(item))"
+    elseif kind == :commit
+        path = "/repos/$(name(repo))/comments/$(name(item))"
+    else
+        error(kind_err_str(kind))
+    end
     return gh_delete(path; options...)
 end
