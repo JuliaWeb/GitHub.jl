aebe27004d7ed2d123dfc55bffddd3d44631c130
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-08-03 01:46:34 +0200

    add back auth

--- test/read_only_api_tests.jl
+++ test/read_only_api_tests.jl
@@ -111,12 +111,12 @@ end
 end
 
 @testset "Gists" begin
-    kc_gists, page_data = gists("KristofferC", page_limit=1, params=Dict("per_page" => 5))
+    kc_gists, page_data = gists("KristofferC"; page_limit=1, params=Dict("per_page" => 5), auth = auth)
     @test typeof(kc_gists) == Vector{Gist}
     @test length(kc_gists) != 0
     @test get(get(kc_gists[1].owner).login) == "KristofferC"
 
-    gist_obj = gist("0cb70f50a28d79905aae907e12cbe58e")
+    gist_obj = gist("0cb70f50a28d79905aae907e12cbe58e"; auth = auth)
     @test length(get(gist_obj.files)) == 2
     @test get(gist_obj.files)["file1.jl"]["content"] == "Hello World!"
 end
