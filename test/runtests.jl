5d60d68561a27222cf0b0b8bbea14578ce41b45e
Tim Holy <tim.holy@gmail.com>
2020-12-17 02:44:06 -0600

    Add ssh keygen (#179)
    This streamlines mass updates of deploy key/secret pairs.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,9 @@ include("ghtype_tests.jl")
 include("event_tests.jl")
 include("read_only_api_tests.jl")
 include("auth_tests.jl")
+
+@testset "SSH keygen" begin
+    pubkey, privkey = GitHub.genkeys(keycomment="GitHub.jl")
+    @test endswith(pubkey, "GitHub.jl")
+    @test isa(privkey, String)
+end
