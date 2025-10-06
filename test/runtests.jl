2dd8848ba67ec178c391da66cd7a65c78917feca
Tim Besard <tim.besard@gmail.com>
2025-10-06 16:34:16 +0200

    CI: Use a custom token (non-fork PRs), or fall back to `GITHUB_TOKEN` (fork PRs) (#207)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,17 @@ using Dates, Test, Base64
 using GitHub: Branch, name
 using GitHub.Checks
 
-include("ghtype_tests.jl")
-include("event_tests.jl")
-include("read_only_api_tests.jl")
-include("auth_tests.jl")
+@testset "GitHub.jl" begin
 
-@testset "SSH keygen" begin
-    pubkey, privkey = GitHub.genkeys(keycomment="GitHub.jl")
-    @test endswith(pubkey, "GitHub.jl")
-    @test isa(privkey, String)
+    include("ghtype_tests.jl")
+    include("event_tests.jl")
+    include("read_only_api_tests.jl")
+    include("auth_tests.jl")
+    
+    @testset "SSH keygen" begin
+        pubkey, privkey = GitHub.genkeys(keycomment="GitHub.jl")
+        @test endswith(pubkey, "GitHub.jl")
+        @test isa(privkey, String)
+    end
+
 end
