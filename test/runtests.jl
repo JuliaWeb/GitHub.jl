395c2f8496f33adbd2822106d045ad1fe17efdf7
Dilum Aluthge <dilum@aluthge.com>
2025-10-06 12:39:00 -0400

    Add some `sleep()`s throughout the test suite, to reduce the chance of getting rate-limited.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,18 @@ using GitHub.Checks
 
 @testset "GitHub.jl" begin
 
+    # We add some `sleep()`s throughout the test suite, to reduce the chance of getting rate-limited.
+
+    sleep(2)
     include("ghtype_tests.jl")
+
+    sleep(2)
     include("event_tests.jl")
+
+    sleep(2)
     include("read_only_api_tests.jl")
+
+    sleep(2)
     include("auth_tests.jl")
     
     @testset "SSH keygen" begin
